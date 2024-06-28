a62f86c5e1f4fc42f397282e7f3faff9573410b5
jérôme <jplusquellec@gmail.com>
2024-06-28 16:32:46 +0200

    vercel

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://vercelbackend-1tbvs30yq-jeromes-projects-6b8e46b2.vercel.app')
+fetch('https://vercelbackend-1tbvs30yq-jeromes-projects-6b8e46b2.vercel.app/year')
  .then(response => response.json())
  .then(data => {
    document.querySelector('#year').textContent = data.year;
